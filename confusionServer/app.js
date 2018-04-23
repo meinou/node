8a1b8ecebd3edf4b87ea655a9d3a1e08f38ad35f
mrs.leonenko <mrs.leonenko@gmail.com>
2018-04-23 19:39:12 -0400

    authentification part0

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -34,65 +38,92 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // function auth (req, res, next) {
-//   console.log(req.headers);
+//  // console.log(req.signedCookies);
+//  console.log(req.session);
 
-//    var authHeader = req.headers.autorization;
-//   if (!authHeader) {
-//     var err = new Error('You are not authenticated!');
-//     res.setHeader('WWW-Authenticate', 'Basic');
-//     err.status = 401;
-//     next(err);
-//     return;
-// }
-//   var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-//   var user = auth[0];
-//   var pass = auth[1];
-//   if (user == 'admin' && pass == 'password') {
-//       next(); // authorized
-//   } else {
+//  // if (!req.signedCookies.user){
+//   if (!req.session.user){
+
+//     // var authHeader = req.headers.authorization;
+
+//   //  if (!authHeader) {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');
+//      //   err.status = 401;
+//         err.status = 403;
+//         next(err);
+//         return;
+//   //  }
+
+//     // var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     // var user = auth[0];
+//     // var pass = auth[1];
+//     // if (user == 'admin' && pass == 'password') {
+//     // //  res.cookie('user', 'admin', {signed: true});
+//     //   req.session.user = 'admin';
+//     //   next(); // authorized
+//     // } else {
+//     //     var err = new Error('You are not authenticated!');
+//     //     res.setHeader('WWW-Authenticate', 'Basic');      
+//     //     err.status = 401;
+//     //     next(err);
+//     // }
+//   } 
+//   else {
+//    // if (req.signedCookies.user === 'admin') {
+//    // if (req.session.user === 'admin') {
+//     if (req.session.user === 'authentificated') {
+//       next();
+//     }
+//     else {
 //       var err = new Error('You are not authenticated!');
-//       res.setHeader('WWW-Authenticate', 'Basic');      
-//       err.status = 401;
+//       err.status = 403;
 //       next(err);
+//       return;
+//     }
 //   }
-//  }
-
-//  app.use(auth);
+// }
 
 function auth (req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
+    console.log(req.session);
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-      next(); // authorized
-  } else {
+  if(!req.session.user) {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
   }
 }
+
 
 app.use(auth);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
